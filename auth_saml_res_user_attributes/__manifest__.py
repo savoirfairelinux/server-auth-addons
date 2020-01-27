de767a89961d16ed4e27f04186b365d6511740e6
Luis <eilstdq@gmail.com>
2020-01-27 13:58:09 -0500

    [ADD] saml mapping name to res users

---
+++ auth_saml_res_user_attributes/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Auth Saml res_user attributes",
+    'summary': """
+        This module extends the functionality of Auth SAML Create User
+        to support the mapping between SAML attributes and odoo res_users.""",
+    'author': "Savoir-faire Linux",
+    'maintainer': 'Savoir-faire Linux',
+    'website': "http://www.yourcompany.com",
+    'license': 'AGPL-3',
+    'category': 'Tools',
+    'version': '11.0.1.0.0',
+    'depends': ['auth_saml_create_user'],
+}
